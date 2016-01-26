b3c88b479a8845fe57eaa55e3e4f9a0e39287c50
Mateusz Wieloch <mwieloch@groupon.com>
2016-01-26 13:57:46 -0600

    Add .rspec

--- 50_ruby.sh
+++ 50_ruby.sh
@@ -7,4 +7,7 @@ brew install rbenv ruby-build
 # This line has to be run each time - running here and adding to .bashrc
 eval "$(rbenv init -)"
 
+echo "# Setup .rspec"
+cp -i .rspec ~
+
 echo "# IMPORTANT: To install a particular version of ruby run $ rbenv install <version>"
