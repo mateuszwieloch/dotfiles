de0a7d3cd443c2c0db7105d71809a664377a1b98
Mateusz Wieloch <mateuszwieloch@outlook.com>
2016-03-01 01:10:54 -0600

    Add fish config files

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,32 @@
+set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_showcolorhints 'yes'
+
+# separator between branch name and flags
+set __fish_git_prompt_char_stateseparator ' '
+set __fish_git_prompt_char_dirtystate '+'
+set __fish_git_prompt_char_invalidstate 'x'
+set __fish_git_prompt_char_stagedstate 's'
+set __fish_git_prompt_char_untrackedfiles 'u'
+
+
+function long_working_dir --description 'Print the current working directory'
+        echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||'
+end
+
+
+function fish_prompt --description 'Write out the prompt'
+  if not test $status -eq 0
+    set -l mode_color red
+  else
+    set -l mode_color normal
+  end
+
+  set_color green
+  printf '%s' (long_working_dir)
+  set_color yellow
+	printf '%s ' (__fish_git_prompt)
+  set_color $mode_color
+  printf '$ '
+
+	set_color normal
+end
