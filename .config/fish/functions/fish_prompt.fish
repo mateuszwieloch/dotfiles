d856f4caea0982b60c5daecd5b5ff39bd3d8f35d
Mateusz Wieloch <mwieloch@groupon.com>
2016-03-02 11:18:56 -0600

    minor config changes

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,6 +2,7 @@ set __fish_git_prompt_show_informative_status 'yes'
 set __fish_git_prompt_showcolorhints 'yes'
 
 # separator between branch name and flags
+# set __fish_git_prompt_color_branch normal
 set __fish_git_prompt_char_stateseparator ' '
 set __fish_git_prompt_char_dirtystate '+'
 set __fish_git_prompt_char_invalidstate 'x'
@@ -10,7 +11,7 @@ set __fish_git_prompt_char_untrackedfiles 'u'
 
 
 function long_working_dir --description 'Print the current working directory'
-        echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||'
+  echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||'
 end
 
 
@@ -23,7 +24,7 @@ function fish_prompt --description 'Write out the prompt'
 
   set_color green
   printf '%s' (long_working_dir)
-  set_color yellow
+  set_color normal
 	printf '%s ' (__fish_git_prompt)
   set_color $mode_color
   printf '$ '
