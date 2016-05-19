151f34c4e3472e784e199a15ce9418df247e9a11
NAME <EMAIL>
2016-05-19 00:14:34 -0500

    fish: convert aliases into abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,21 @@ status --is-interactive; and . (rbenv init -|psub)
 set -x LSCOLORS gxBxhxDxfxhxhxhxhxcxcx
 set -x LS_COLORS "di=36;40:ln=1;31;40:so=37;40:pi=1;33;40:ex=35;40:bd=37;40:cd=37;40:su=37;40:sg=37;40:tw=32;40:ow=32;40:"
 
-alias be 'bundle exec'
-alias cp 'cp -i'
-alias mv 'mv -i'
-alias rm 'rm -i'
-alias lsa 'ls -a'
-alias lla 'll -a'
-alias sl 'll -1'
-alias sla 'lla -1'
-
 set -g CDPATH . ~
 if test -d ~/code
   set -g CDPATH $CDPATH ~/code
 end
+
+set -U fish_user_abbreviations 'be=bundle exec'
+
+set fish_user_abbreviations $fish_user_abbreviations 'cp=cp -i'
+set fish_user_abbreviations $fish_user_abbreviations 'mv=mv -i'
+set fish_user_abbreviations $fish_user_abbreviations 'rm=rm -i'
+
+set fish_user_abbreviations $fish_user_abbreviations 'lsa=ls -a'
+set fish_user_abbreviations $fish_user_abbreviations 'lla=ll -a'
+set fish_user_abbreviations $fish_user_abbreviations 'sl=ll -1'
+set fish_user_abbreviations $fish_user_abbreviations 'sla=ll -a -1'
+
+set fish_user_abbreviations $fish_user_abbreviations 'g=git'
+set fish_user_abbreviations $fish_user_abbreviations 'gs=git status -s'
