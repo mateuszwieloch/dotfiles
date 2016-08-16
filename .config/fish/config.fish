1fd0dcaadd043dde1d6c31f43b19c3e2ee0bdfcf
Mateusz Wieloch <mwieloch@groupon.com>
2016-08-16 10:27:39 -0500

    Add gito and gitpr ruby scripts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,10 @@
 status --is-interactive; and . (rbenv init -|psub)
 
+
 set -x LSCOLORS gxBxhxDxfxhxhxhxhxcxcx
 set -x LS_COLORS "di=36;40:ln=1;31;40:so=37;40:pi=1;33;40:ex=35;40:bd=37;40:cd=37;40:su=37;40:sg=37;40:tw=32;40:ow=32;40:"
+
+set -gx PATH $PATH ~/code/dotfiles/bin
 
 set -g CDPATH . ~
 if test -d ~/code
