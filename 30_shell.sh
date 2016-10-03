e6b4f41f0b30f67e058957b9328c2232c0d4cab3
Mateusz Wieloch <mwieloch@groupon.com>
2016-10-03 17:04:03 -0500

    Add docker completion to fish

---
+++ 30_shell.sh
@@ -0,0 +1,28 @@
+# BASH
+echo "# Install latest bash (4.x)"
+brew install bash
+
+echo "# Append bash (4.x) to /etc/shells"
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+
+echo "# Setup .bashrc and .profile"
+cp -i .bashrc .profile ~
+
+echo "# Setup .git-completion.bash"
+cp -i .git-completion.bash ~
+
+
+
+# FISH
+echo "# Install fish-shell"
+brew install fish
+
+echo "# Append fish to /etc/shells"
+sudo bash -c 'echo /usr/local/bin/fish >> /etc/shells'
+
+echo "# Change to the new shell"
+chsh -s /usr/local/bin/fish
+
+# docker completion for fish
+mkdir ~/.config/fish/completions
+wget https://raw.github.com/barnybug/docker-fish-completion/master/docker.fish -O ~/.config/fish/completions/docker.fish
---
+++ 35_git.sh
@@ -0,0 +1,8 @@
+# Git
+echo "# Install git"
+brew install git
+
+echo "# Setup .gitconfig"
+cp -i .gitconfig ~
+
+echo "# IMPORTANT: set your user name and email in ~/.gitconfig"
