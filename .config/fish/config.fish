3444638dd8209081641863df7691119f5392cf4e
Mateusz Wieloch <mwieloch@groupon.com>
2016-12-13 15:51:13 -0600

    fish: add support for gateway tooling

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ status --is-interactive; and . (rbenv init -|psub)
 set -x LSCOLORS gxBxhxDxfxhxhxhxhxcxcx
 set -x LS_COLORS "di=36;40:ln=1;31;40:so=37;40:pi=1;33;40:ex=35;40:bd=37;40:cd=37;40:su=37;40:sg=37;40:tw=32;40:ow=32;40:"
 
-set -gx PATH $PATH ~/code/dotfiles/bin
+set -gx PATH $PATH ~/code/dotfiles/bin ~/code/gateway/public_bin
 
 set -g CDPATH . ~
 if test -d ~/code
@@ -29,8 +29,5 @@ set fish_user_abbreviations $fish_user_abbreviations 'lsa=ls -a'
 set fish_user_abbreviations $fish_user_abbreviations 'lla=ll -a'
 set fish_user_abbreviations $fish_user_abbreviations 'sl=ll -1'
 set fish_user_abbreviations $fish_user_abbreviations 'sla=ll -a -1'
-
-set fish_user_abbreviations $fish_user_abbreviations 'g=git'
-set fish_user_abbreviations $fish_user_abbreviations 'gs=git status -s'
 
 set fish_user_abbreviations $fish_user_abbreviations 'py=python3'
