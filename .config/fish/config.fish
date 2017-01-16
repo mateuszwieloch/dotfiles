76ebd3548e395682413d931c4e685717842633df
Mateusz Wieloch <mateuszwieloch@gmail.com>
2017-01-16 15:10:12 -0600

    fish: only add Gateway's binaries to PATH for work laptop

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,10 @@ status --is-interactive; and . (rbenv init -|psub)
 set -x LSCOLORS gxBxhxDxfxhxhxhxhxcxcx
 set -x LS_COLORS "di=36;40:ln=1;31;40:so=37;40:pi=1;33;40:ex=35;40:bd=37;40:cd=37;40:su=37;40:sg=37;40:tw=32;40:ow=32;40:"
 
-set -gx PATH $PATH ~/code/dotfiles/bin ~/code/gateway/public_bin
+set -gx PATH $PATH ~/code/dotfiles/bin
+if test -d ~/code/gateway
+  set -gx PATH $PATH ~/code/gateway/public_bin
+end
 
 set -g CDPATH . ~
 # if ~/code exists add all subdirectories of ~/code to fold autocomplete list triggered with TAB
