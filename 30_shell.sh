eca130b1f6a787dd2089aaef9c9c4f9a40f724d1
Mateusz Wieloch <mwieloch@uber.com>
2019-09-04 22:47:08 -0700

    fish: add completions for docker

--- 30_shell.sh
+++ 30_shell.sh
@@ -6,9 +6,7 @@ brew install fish
 sudo bash -c 'echo /usr/local/bin/fish >> /etc/shells'
 chsh -s /usr/local/bin/fish
 
-# docker completion for fish
-mkdir -p ~/.config/fish/completions
-wget https://raw.github.com/barnybug/docker-fish-completion/master/docker.fish -O ~/.config/fish/completions/docker.fish
+ln -s ~/code/dotfiles/.config/fish/completions ~/.config/fish/completions
 
 # Symlink config files
 mkdir -p ~/.config/fish/functions
