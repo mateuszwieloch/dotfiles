5288c550197e2ec9371a5e5318c2e47a945d390e
Mateusz Wieloch <mwieloch@uber.com>
2019-12-05 21:32:42 -0800

    ruby: add .rubocop.yml to tracked files

--- 50_ruby.sh
+++ 50_ruby.sh
@@ -8,5 +8,6 @@ ln -s ~/code/dotfiles/.rspec ~/.rspec
 ln -s ~/code/dotfiles/.irbrc ~/.irbrc
 ln -s ~/code/dotfiles/.gemrc ~/.gemrc
 ln -s ~/code/dotfiles/.pryrc ~/.pryrc
+ln -s ~/code/dotfiles/.rubocop.yml ~/.rubocop.yml
 
 echo "# IMPORTANT: To install a particular version of ruby run $ rbenv install <version>"
