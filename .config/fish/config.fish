5b515139530c35b90e1771f83911fcbaf663abf2
Mateusz Wieloch <github@mateuszwieloch.com>
2021-02-04 15:18:27 -0800

    macos: Tanium customizations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ set -x LSCOLORS xxBxhxDxfxhxhxhxhxcxcx
 # Linux version
 set -x LS_COLORS "di=00;40:ln=1;31;40:so=37;40:pi=1;33;40:ex=35;40:bd=37;40:cd=37;40:su=37;40:sg=37;40:tw=32;40:ow=32;40:"
 
+
 # PATH
 set -l paths \
 ~/code/dotfiles/bin \
@@ -16,7 +17,10 @@ set -l paths \
 ~/bin \
 ~/.local/bin \
 /usr/local/sbin \
-/usr/local/opt/coreutils/bin
+/usr/local/opt/coreutils/bin \
+~/.okta/bin \
+~/code/tanium-cloud/SaaS-CustomerEnvironment/cli \
+~/code/tanium-cloud/sre-tools/bin
 
 for p in $paths
   if not contains $p $PATH; and test -d $p
@@ -25,11 +29,17 @@ for p in $paths
   end
 end
 
+# append
+set -gx PATH $PATH .
+
+
 # CDPATH - list of file and folder name auto-completions triggered with TAB
 set -l cdpaths \
 . \
 ~ \
-~/code
+~/code \
+~/code/tanium-cloud \
+~/code/tanium
 
 for p in $cdpaths
   if not contains $p $CDPATH; and test -d $p
@@ -38,9 +48,16 @@ for p in $cdpaths
   end
 end
 
+
+# fnm
+fnm env | source
+
 # for GPG to work with Fish
-set -x GPG_TTY (tty)
+set --export GPG_TTY (tty)
 
 # PYTHON
 # pipx completions
 register-python-argcomplete --shell fish pipx | source
+
+# Tanium - for metrics-all-in-one-env
+set --export TS_IP '192.168.56.80'
