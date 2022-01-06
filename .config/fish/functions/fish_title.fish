692b8364d160c5f3b97a45963075bc41e31fec83
Mateusz Wieloch <github@mateuszwieloch.com>
2022-01-06 14:24:01 +0100

    kitty: fix 0.24 overriding user-defined fish functions

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,3 +1,5 @@
+# The output is used as a tab name
+# The first argument to fish_title contains the most recently executed foreground command as a string, if any.
 function fish_title
-    echo (basename $PWD)
+  echo (basename $PWD)
 end
