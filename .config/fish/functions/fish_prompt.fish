f45d27ede88cc0ec8f52433232cab0e730925258
Mateusz Wieloch <github@mateuszwieloch.com>
2022-11-07 23:03:00 +0100

    fish: fix git status

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -14,6 +14,7 @@ set __fish_git_prompt_color_stagedstate 'yellow'
 set __fish_git_prompt_char_dirtystate '+'
 set __fish_git_prompt_color_dirtystate 'F00'
 
+set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_char_untrackedfiles 'u'
 set __fish_git_prompt_color_untrackedfiles 'magenta'
 
@@ -27,12 +28,6 @@ end
 
 # ~/path/to/dir master↑3↓2 s1+4 $
 function fish_prompt --description 'Write out the prompt'
-  if not test $status -eq 0
-    set -l mode_color red
-  else
-    set -l mode_color normal
-  end
-
   set_color yellow
   printf '%s ' (long_working_dir)
   set_color normal
