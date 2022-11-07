1a1e61dc3b990fc9a40947e7fa2668dbdcb2e34d
Mateusz Wieloch <github@mateuszwieloch.com>
2022-11-07 14:48:01 +0100

    ubuntu: add FiraMono Nerd Font

---
+++ ubuntu/fonts.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/fish
+
+sudo apt-get install font-manager
+
+sudo http --download 'https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraMono.zip' --output="$HOME/Downloads/FiraMono.zip"
+
+set_color black --background=yellow
+echo
+echo
+echo "Now open the Font Manager app and add the ~/Downloads/FiraMono.zip font."
+echo "Confusingly the actual font name is FuraMono Nerd Font."
+set_color normal
