5b095614571cf9c7322bbd3d4d2600fdb2ae9b47
Mateusz Wieloch <github@mateuszwieloch.com>
2022-11-13 20:10:26 +0100

    ubuntu: add pCloud

---
+++ ubuntu/pcloud.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+
+# dependencies
+sudo add-apt-repository universe
+sudo apt install libfuse2
+
+echo "Manually download (cannot figure out how to auto-download): https://www.pcloud.com/how-to-install-pcloud-drive-linux.html?download=electron-64#"
+echo "Put it in ~/bin folder"
+
