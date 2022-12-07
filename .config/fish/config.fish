abdbbfe0d4421f8e0e9a52690693fffddcefb6cb
Mateusz Wieloch <github@mateuszwieloch.com>
2022-12-07 11:49:59 +0100

    cli: fix exa colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,6 @@
 set -x EDITOR vim
 set -gx RIPGREP_CONFIG_PATH ~/.ripgreprc
 
-if not command -sq exa
-  # generated with https://geoff.greer.fm/lscolors/
-  # BSD version
-  set -x LSCOLORS xxBxhxDxfxhxhxhxhxcxcx
-  # Linux version
-  set -x LS_COLORS "di=00;40:ln=1;31;40:so=37;40:pi=1;33;40:ex=35;40:bd=37;40:cd=37;40:su=37;40:sg=37;40:tw=32;40:ow=32;40:"
-end
-
-set --export BAT_STYLE numbers
-set --export BAT_THEME "Monokai Extended"
-
 
 # PATH
 set -l paths \
@@ -77,3 +66,30 @@ source ~/.config/fish/local/*.fish
 
 # Detect if Homebrew was installed on Linux and source shell env
 test -d /home/linuxbrew/.linuxbrew && eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+
+if command -sq exa
+  # Overall syntax: https://the.exa.website/docs/colour-themes
+  # Color codes: https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
+
+  # 38;5  means select foreground color
+  # 48;5  means select background color
+  # next goes 8bit color code
+  # next can be style 1=bold 4=underline
+  # combined example: EXA_COLORS "*.xyz=38;5;14;1"
+
+  # di = directories
+  set -x EXA_COLORS (string join ':' \
+    "di=38;5;37;1" \
+    "*.js=38;5;220"
+  )
+else
+  # generated with https://geoff.greer.fm/lscolors/
+  # BSD version
+  set -x LSCOLORS xxBxhxDxfxhxhxhxhxcxcx
+  # Linux version
+  set -x LS_COLORS "di=00;40:ln=1;31;40:so=37;40:pi=1;33;40:ex=35;40:bd=37;40:cd=37;40:su=37;40:sg=37;40:tw=32;40:ow=32;40:"
+end
+
+set --export BAT_STYLE numbers
+set --export BAT_THEME "Monokai Extended"
