85d1d445126322d626f90ba1d893dd7cc130f04a
Mateusz Wieloch <github@mateuszwieloch.com>
2022-12-20 12:35:09 +0100

    ubuntu: add keyd to manage keyboard mappings

--- ubuntu/10_keyd.sh
+++ ubuntu/10_keyd.sh
@@ -7,3 +7,10 @@ sudo systemctl enable keyd && sudo systemctl start keyd
 popd
 
 sudo ln -sf ~/code/mateuszwieloch/dotfiles/ubuntu/keyd/default.conf /etc/keyd/default.conf
+
+sudo usermod -aG keyd $(whoami)
+mkdir -p ~/.config/keyd
+sudo ln -sf ~/code/mateuszwieloch/dotfiles/ubuntu/keyd/app.conf ~/.config/keyd/app.conf
+set_color black --background=yellow
+echo "The next command should install keyd GNOME extension. Verify that it was added. Enable if needed."
+keyd-application-mapper
