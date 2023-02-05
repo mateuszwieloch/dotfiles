3059066d2e23f43b79c07e25f7fc611f635df6d4
Mateusz Wieloch <github@mateuszwieloch.com>
2023-02-05 21:04:23 +0100

    ubuntu: add Crystal and icr installation script

---
+++ ubuntu/crystal.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+
+echo "ASDF does not support crystal with icr or crystalline LSP yet"
+brew install crystal
+brew install crystal-icr
+
+echo "See this file if you see the 'error while loading shared libraries libgc.so.1'"
+# sudo apt-get install libgc-dev
