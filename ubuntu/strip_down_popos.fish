a6e7f2b9f31ca0ebc8fbd6bee73aeab007fe3b56
Mateusz Wieloch <9206426+mateuszwieloch@users.noreply.github.com>
2023-02-26 23:10:40 +0100

    ubuntu: remove french and chinese language packs

--- ubuntu/strip_down_popos.fish
+++ ubuntu/strip_down_popos.fish
@@ -6,3 +6,7 @@ sudo apt remove brltty
 sudo apt remove geary
 # video player
 sudo apt remove totem totem-plugins totem-common
+
+sudo apt remove language-pack-fr-base language-pack-fr language-pack-gnome-fr-base language-pack-gnome-fr
+sudo apt remove language-pack-zh-hans-base language-pack-zh-hans language-pack-gnome-zh-hans-base language-pack-gnome-zh-hans
+sudo apt remove language-pack-zh-hant-base language-pack-zh-hant language-pack-gnome-zh-hant-base language-pack-gnome-zh-hant
