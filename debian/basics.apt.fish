8b30c2d9e58376af62acbaa3708d1b73d8b88000
Mateusz Wieloch <9206426+mateuszwieloch@users.noreply.github.com>
2024-04-24 22:47:50 +0200

    debian: symlink fdfind to fd

--- debian/basics.apt.fish
+++ debian/basics.apt.fish
@@ -9,8 +9,6 @@ sudo apt-get install curl \
   # A lot of dev apps require a C compiler
   build-essential \
   wget \
-  # find replacement
-  fd-find \
   # sed replacement
   sd \
   tree \
@@ -21,10 +19,14 @@ sudo apt-get install curl \
 sudo apt-get install ripgrep \
   && sudo ln -sf ~/code/mateuszwieloch/dotfiles/.ripgreprc  ~/.ripgreprc
 
+# find replacement
+sudo apt-get install fd-find \
+  && sudo ln -sf (which fdfind) /usr/bin/fd
+
 # cat replacement with syntax highlighting and Git integration
 # https://github.com/sharkdp/bat
 sudo apt-get install bat \
-  && sudo ln -sf /usr/bin/batcat /usr/bin/bat
+  && sudo ln -sf (which batcat) /usr/bin/bat
 
 # eza - a ls replacement. I use it in ll.fish. It's only available in Debian Unstable or from a custom source.
 sudo mkdir -p /etc/apt/keyrings
