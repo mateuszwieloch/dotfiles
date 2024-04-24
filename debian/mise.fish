9aaf00b9c312787576cf7c59705f4dc666f7a2db
Mateusz Wieloch <9206426+mateuszwieloch@users.noreply.github.com>
2024-04-24 22:39:33 +0200

    debian(mise): add mise Fish completions, update config

--- debian/mise.fish
+++ debian/mise.fish
@@ -5,3 +5,7 @@ curl https://mise.jdx.dev/install.sh | sh
 
 ln -s ~/code/mateuszwieloch/dotfiles/.config/mise/settings.toml ~/.config/mise/settings.toml
 ln -s ~/code/mateuszwieloch/dotfiles/.default-python-packages ~/.default-python-packages
+
+# Install Fish shell completions
+mise use -g usage
+mise completion fish > ~/.config/fish/completions/mise.fish
