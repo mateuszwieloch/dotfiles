75ec7d8325358483a05bd25f6a26fcb6732ed7bb
Mateusz Wieloch <9206426+mateuszwieloch@users.noreply.github.com>
2024-07-29 14:07:55 +0200

    windows: update wslu install script

--- windows/wslu.apt.fish
+++ windows/wslu.apt.fish
@@ -2,6 +2,13 @@
 
 set fish_trace 1
 
+sudo apt install gnupg2 apt-transport-https
+wget -O - https://pkg.wslutiliti.es/public.key | sudo gpg -o /usr/share/keyrings/wslu-archive-keyring.pgp --dearmor
+echo "deb [signed-by=/usr/share/keyrings/wslu-archive-keyring.pgp] https://pkg.wslutiliti.es/debian bookworm main" | sudo tee /etc/apt/sources.list.d/wslu.list
+
+sudo apt update
+
+
 # These commands allow to open html link in Windows browser from WSL
 sudo apt install wslu
 set --export --universal BROWSER "wslview"
