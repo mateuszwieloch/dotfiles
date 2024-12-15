fdc8ca1d40959a0d41b7ee96540382149730b8c9
Mateusz Wieloch <9206426+mateuszwieloch@users.noreply.github.com>
2024-12-15 23:42:29 +0100

    vim(ft): Show whitespace chars for Makefile

---
+++ .config/nvim/after/ftplugin/make.lua
@@ -0,0 +1,4 @@
+vim.opt_local.expandtab = false
+vim.opt.tabstop = 4       -- number of spaces a <Tab> in the text stands for
+vim.opt_local.list = true
+vim.opt_local.listchars = 'tab:➜ ,lead:•,trail:•'
