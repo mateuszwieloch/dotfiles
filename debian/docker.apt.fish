39247d141a9cc233497965ad8ee2db094b4e6e62
Mateusz Wieloch <9206426+mateuszwieloch@users.noreply.github.com>
2025-02-03 00:04:55 +0100

    debian: Install Docker

---
+++ debian/docker.apt.fish
@@ -0,0 +1,29 @@
+#!/usr/bin/env fish
+set fish_trace 1
+
+# Prerequisites
+sudo apt-get install ca-certificates curl
+
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Download GPG key and store repository in the system
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" |tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+# Optional packages
+# sudo apt-get install docker-buildx-plugin docker-compose-plugin
+
+echo
+echo "You can now check whether docker is active by running `systemctl is-active docker` command"
