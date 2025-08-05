da315e4c39f6b663a29a2e4915a00977e857d74e
Mateusz Wieloch <9206426+mateuszwieloch@users.noreply.github.com>
2025-08-05 09:40:00 +0200

    git: Global ignore for mise.local.toml

--- git.sh
+++ git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo 'Symlinking .gitconfig and .gitconfig-bp'
+echo 'Symlinking .gitconfig, .gitconfig-bp, .gitignore_global'
 set -x
 ln -s ~/code/mateuszwieloch/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/code/mateuszwieloch/dotfiles/.gitconfig-bp ~/.gitconfig-bp
+ln -s ~/code/mateuszwieloch/dotfiles/.gitignore_global ~/.gitignore_global
